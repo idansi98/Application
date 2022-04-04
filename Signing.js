a62e153910614ff16b4c7d49a1c2f8a4a4fc0958
idansi98 <idansi98@gmail.com>
2022-04-04 09:28:33 +0300

    ך

--- Signing.js
+++ Signing.js
@@ -1,15 +1,23 @@
 users = [];
-var check = document.getElementById("Login");
+var check = document.getElementById("Register");
 check.onclick = function() {
     //"if(isPasswordValid(password) && password == passwordAgain)
         let user = {
-            "Username" : getElementById("Username").value,
-            "Password" : getElementById("Password").value,
-            "Verifypassword" : getElementById("Verifypassword").value,
-            "Displayname" : getElementById("Displayname").value,
-            "Photo" : getElementById("Photo").value
+            "Username" : document.getElementById("Username").value,
+            "Password" : document.getElementById("Password").value,
+            "Verifypassword" : document.getElementById("Verifypassword").value,
+            "Displayname" : document.getElementById("Displayname").value,
+            "Photo" : document.getElementById("Photo").value
         }
         users.push(user);
-        console.login(users[0]);
+        printArray(users);
     }
+
+    function printArray(p) {
+        for(let i = 0; i < p.length; i++) {
+            console.log(p[i].Username);
+        }
+    }
+    
+    
 
