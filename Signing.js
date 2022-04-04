a764e9f28a283abcb06be29637db29dc98e57d3f
idansi98 <idansi98@gmail.com>
2022-04-04 13:50:47 +0300

    חמי

--- Signing.js
+++ Signing.js
@@ -1,23 +1,52 @@
 users = [];
 var check = document.getElementById("Register");
 check.onclick = function() {
-    //"if(isPasswordValid(password) && password == passwordAgain)
-        let user = {
-            "Username" : document.getElementById("Username").value,
-            "Password" : document.getElementById("Password").value,
-            "Verifypassword" : document.getElementById("Verifypassword").value,
-            "Displayname" : document.getElementById("Displayname").value,
-            "Photo" : document.getElementById("Photo").value
+    var password = document.getElementById("Password").value;
+    if(isPasswordValid(password) && isPasswordValidLength(password)) {
+    let user = {
+        "Username" : document.getElementById("Username").value,
+        "Password" : password,
+        "Verifypassword" : document.getElementById("Verifypassword").value,
+        "Displayname" : document.getElementById("Displayname").value,
+        "Photo" : document.getElementById("Photo").value
         }
         users.push(user);
         printArray(users);
     }
+}
+
 
     function printArray(p) {
         for(let i = 0; i < p.length; i++) {
             console.log(p[i].Username);
         }
     }
-    
-    
 
+    function isPasswordValidLength(password) {
+        if(password.length < 8) {
+            console.log("Minimum length of password is 8 characters, please try again!");
+            return false;        
+        }
+        if(password.length > 20) {
+            console.log("Maximum length of password is 20 characters, please try again!");
+            return false; 
+        }
+    }
+
+    function isPasswordValid(password) {
+        var numChecker = false;
+        var letterChecker = false;
+        for(let i = 0; i < password.length; i++) {
+            if(password[i].toUpperCase() != password[i].toLowerCase()) {
+                letterChecker = true;
+            }
+            if(password[i] >= '0' && password[i] <= '9') {
+                numChecker = true;
+            }
+        }
+        if(numChecker && letterChecker) {
+            return true;
+        }
+        return false;    
+    }
+    
